038b0e518c100a3fd4ac29d88498aa2abc074430
Sai Pooja Mahajan <saipooja@gmail.com>
2020-10-16 09:24:21 -0400

    cleaup

--- functions_AAprops.py
+++ functions_AAprops.py
@@ -10,27 +10,13 @@ from rosetta.protocols.docking import *
 import argparse
 from rosetta.core.pose import *
 import math
-from rosetta.core.simple_metrics import metrics
-'''
-Ideal dihedrals
-498     -140.04573469   175.091374176   177.960573017   obj_2ffu_cleanedup_0001_glycosylated_alpha_0001_0003.pdb
-499     -70.0465905099  123.07621314    -179.753840373  obj_2ffu_cleanedup_0001_glycosylated_alpha_0001_0003.pdb
-500     -74.237453751   150.645718028   178.553135172   obj_2ffu_cleanedup_0001_glycosylated_alpha_0001_0003.pdb
-'''
-PI = math.pi
-phi_ref = {498:-140.04573469, 499:-70.0465905099, 500:-74.237453751}
-psi_ref = {498:175.091374176, 499:123.07621314, 500:150.645718028}
-omega_ref = {498:177.960573017, 499:-179.753840373, 500:178.553135172}
 
-residue_xp1 = 500
+PI = math.pi
 calculatermsd=False
 scriptmode=False
 verbose=False
 dihedrals = ["phi","psi","omega"]
 
-resdict1  = {'HWUO1B-THR7N' : [495 , 499]  , 'HWUO1-THR7N' : [495 , 499] }
-resdict2 = {'UDP2OPB-THR7N' : [496, 499] , 'UDP3OPB-THR7N' : [496, 499]}
-resdict = resdict2
 atomdict = {'HWUO1B-THR7N' : ['O1B' , 'N'] , 'HWUO1-THR7N' : ['O1\'' , 'N'], 'UDP2OPB-THR7N' : ['2OPB','N'] , 'UDP3OPB-THR7N' : ['3OPB', 'N'] }
 
 
@@ -137,59 +123,6 @@ def getdihedralforpose(pose,residue):
         return phi,psi,omega
 
 
-def getdihedrals_for_file(f, residues=[498,499,500],calculatermsd=False):
-                propertiesdict = dict()
-                pose = pose_from_pdb(f)
-                rms = 0.0
-                rms_omega = 0.0
-                rms_xp1 = 0.0
-                rms_xp1_omega = 0.0
-                for residue in residues:
-                        phi,psi,omega = getdihedralforpose(pose,residue)
-                        if calculatermsd:
-                                rms += (2 * (1- math.cos ((phi-phi_ref[residue])*PI/180.0)))
-                                rms += (2 * (1- math.cos ((psi-psi_ref[residue])*PI/180.0)))
-                                rms_omega += (2 * (1- math.cos ((omega-omega_ref[residue])*PI/180.0)))
-                                if residue==residue_xp1:
-                                        rms_xp1 += (2 * (1- math.cos ((phi-phi_ref[residue])*PI/180.0)))
-                                        rms_xp1 += (2 * (1- math.cos ((psi-psi_ref[residue])*PI/180.0)))
-                                        rms_xp1_omega += (2 * (1- math.cos ((omega-omega_ref[residue])*PI/180.0)))
-                        propertiesdict["phi_" + str(residue)] = phi
-                        propertiesdict["psi_" + str(residue)] = psi
-                        propertiesdict["omega_" + str(residue)] = omega
-                if calculatermsd:
-                        rms_omega += rms
-                        rms_omega /= float(len(residues))
-
-                        rms /= float(len(residues))
-
-                        rms_xp1_omega += rms_xp1 #xp1 is simgle residue no need to normalize
-
-
-                        propertiesdict["rmsddih"] = rms
-                        propertiesdict["rmsddih_omega"] = rms_omega
-                        propertiesdict["rmsddih_xp1"] = rms_xp1
-                        propertiesdict["rmsddih_xp1_omega"] = rms_xp1_omega
-                return propertiesdict
-
-def getdihdrals(files, residues=[498,499,500],calculatermsd=False):
-  propertiesdict = dict()
-  for residue in residues:
-    for dih in dihedrals:
-      propertiesdict["%s_%d" %(dih,residue)]=[]
-
-  if calculatermsd:
-    propertiesdict['rmsddih']=[]
-    propertiesdict['rmsddih_omega']=[]
-    propertiesdict['rmsddih_xp1']=[]
-    propertiesdict['rmsddih_xp1_omega']=[]
-  for curfile in files:
-    pdict = getdihedrals_for_file(curfile,residues,calculatermsd=False)
-    for key in pdict:
-      propertiesdict[key].append(pdict[key])
-
-  return propertiesdict
-  
 			
 def CalculateRMSDCA(pose_mod,pose_ref):
   rmsd = CA_rmsd(pose_mod,pose_ref)
@@ -248,29 +181,4 @@ def get_fnat_and_fnotnat(file1,file2='T2_with_UDPGalNAc_withS_from_4d0z_ATTAAPRC
   dict_['fnonnat']=fnonnat
   return dict_
 
-if __name__ == '__main__':
-  #pose_ref = pose_from_pdb(sys.argv[2])
-  pose_mod = pose_from_pdb(sys.argv[1])
-  pose = pose_mod
-  sf = ScoreFunction()
-  sf = get_fa_scorefxn()
-  #d = getdistanceforfile_thrbkhbond(sys.argv[1])
-  d =getdistanceforfile_plusone(sys.argv[1])
-  print(d)
-  #setup_foldtree(pose_ref,'AB_P', Vector1([1]))
-  #setup_foldtree(pose,'AB_P', Vector1([1]))
-  
-  #fnat = pyrosetta.rosetta.protocols.docking.calc_Fnat(pose,pose_ref,sf,Vector1([1]))
-  #print(fnat)
-  #results = get_sc_for_pose(pose_mod)
-  #print('SC results ',results.sc,results.area,results.distance)
-  #dict_ = get_sc_for_file(sys.argv[1])
-  #print(dict_)
-  #CalculateSASA(pose_mod)
-  #pose_ref = pose_from_pdb(sys.argv[2])
-  #rmsd =CalculateRMSDCA(pose_mod,pose_ref)
-  #print(rmsd)
-  #chain1 = pose_mod.chain(480)
-  #chain2 = pose_mod.chain(500)
-  #print(chain1,chain2)
   
